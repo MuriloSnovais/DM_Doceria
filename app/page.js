76658c58138038c25ba1573380af2b974a46ab90
mariaeloiza <elooiza1@gmail.com>
2025-03-18 17:07:06 -0300

    mudando o nome da guia

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Image from "next/image";
 import React from "react"
 import MenuSuperior from './components/MenuSuperior';
@@ -9,11 +8,6 @@ const Home = () => {
 
   return (
     <div>
-      <Head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>DM Doceria</title>
-      </Head>
 
       <MenuSuperior />
       <Categorias />
