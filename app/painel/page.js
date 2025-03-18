5ec1bd45d3b298796dabf72780fa01435eb452d2
Rafael <rafadedeus2020@gmail.com>
2025-03-18 17:13:04 -0300

    Add painel

---
+++ app/painel/page.js
@@ -0,0 +1,69 @@
+'use client'
+
+import "./painel.css"
+
+function Painel() {
+    return ( 
+        <div>
+
+            <div >
+
+                
+
+            </div>
+
+
+
+
+            <h1>Página do Administrador</h1>
+            <p className="">
+                <img src="https://placehold.co/100x100" alt="imagem de perfil"></img> 
+                <br/><a href="https://chrome.google.com/webstore?hl=pt-BR">Perfil</a> <hr/>
+            </p>
+            <ul >
+
+                <li><a href="https://www.youtube.com/" target="_blank">Home</a></li>
+                <li><a href="https://chromewebstore.google.com/" target="_blank">Pedidos</a></li>
+                <li><a href="https://chrome.google.com/webstore?hl=pt-BR" target="_blank" >Dashboard</a> </li>
+                
+            </ul>
+        
+            <div className="prioridades">
+
+                <h3>Prioridade</h3>
+                <p>Pedido/data</p>
+                <img src="https://placehold.co/100x100"></img>
+                
+
+                <div>
+                    <h3>Prioridade</h3>
+                    <p>Pedido/data</p>
+                    <img src="https://placehold.co/100x100"></img>
+                </div>
+
+                <div>
+                    <h3>Prioridade</h3>
+                    <p>Pedido/data</p>
+                    <img src="https://placehold.co/100x100"></img>
+                </div>
+
+                <div>
+                    <h3>Prioridade</h3>
+                    <p>Pedido/data</p>
+                    <img src="https://placehold.co/100x100"></img>
+                </div>
+                
+                <div>
+                    <h3>Prioridade</h3>
+                    <p>Pedido/data</p>
+                    <img src="https://placehold.co/100x100"></img>
+                </div>
+            </div>
+        </div>
+
+        
+    
+     );
+}
+
+export default Painel;
