7be771006ad68f0a40934ec2001fb0d4ee5241df
Rafael <rafadedeus2020@gmail.com>
2025-03-19 17:07:43 -0300

    atualização painel

--- app/painel/page.js
+++ app/painel/page.js
@@ -7,32 +7,56 @@ function Painel() {
     return ( 
         <div>
 
-            <div >
+        
+            <div className="cabecalho">
+
+                <h1>Página do Administrador</h1>
 
                 
+            
+                <ul>
+                    <strong>
+                    <li><button onClick={()=> redirect("./login")}>Home</button></li>
+                    <li><button onClick={()=> redirect("./cadastro")}>Pedidos</button></li>
+                    <li><button onClick={()=> redirect("./pagamento")}>Dashboard</button></li>
+                    </strong>
+                </ul>
 
             </div>
 
+            <div className="">
+                    <img src="https://placehold.co/100x100" alt="imagem de perfil"></img><br/>
+                    <button onClick={()=> redirect("./carrinho")}>Perfil</button>
 
+                </div>
 
-
-            <h1>Página do Administrador</h1>
-            <p className="">
-                <img src="https://placehold.co/100x100" alt="imagem de perfil"></img><br/>
-                <button onClick={()=> redirect("./carrinho")}>Perfil</button>
-
-            </p>
-            
-            <ul >
-                <strong>
-                <li><a href="https://www.youtube.com/" target="_blank">Home</a></li>
-                <li><a href="https://chromewebstore.google.com/" target="_blank">Pedidos</a></li>
-                <li><a href="https://chrome.google.com/webstore?hl=pt-BR" target="_blank" >Dashboard</a> </li>
-                </strong>
-            </ul>
-        
             <div className="prioridades">
 
+                <div className="prioridade">
+                    <h3>Prioridade</h3>
+                    <p>Pedido/data</p>
+                    <img src="https://placehold.co/100x100"></img>
+                </div>
+                <div className="prioridade">
+                    <h3>Prioridade</h3>
+                    <p>Pedido/data</p>
+                    <img src="https://placehold.co/100x100"></img>
+                </div>
+                <div className="prioridade">
+                    <h3>Prioridade</h3>
+                    <p>Pedido/data</p>
+                    <img src="https://placehold.co/100x100"></img>
+                </div>
+                <div className="prioridade">
+                    <h3>Prioridade</h3>
+                    <p>Pedido/data</p>
+                    <img src="https://placehold.co/100x100"></img>
+                </div>
+                <div className="prioridade">
+                    <h3>Prioridade</h3>
+                    <p>Pedido/data</p>
+                    <img src="https://placehold.co/100x100"></img>
+                </div>
                 <div className="prioridade">
                     <h3>Prioridade</h3>
                     <p>Pedido/data</p>
